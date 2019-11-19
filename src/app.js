18a3b044ff054f909e81391b32a6548eb77a78cd
webpersonalserver <web_youxiang@163.com>
2019-11-19 20:34:59 +0800

    添加我的页面逻辑

--- src/app.js
+++ src/app.js
@@ -1,7 +1,40 @@
 //app.js
 App({
   data: {},
-  globalData: {},
-  onLaunch: function () {}
+  globalData: {
+    hasLogin: false, // 是否登录，true：已登录；false：未登录
+    hasAuthorize: false, // 是否授权，true：已授权；false：未授权
+    wxUserInfo: {} // 微信用户信息
+  },
+  onLaunch () {
+    this.initProgram()
+  },
+  initProgram () {
+    return new Promise((resolve, reject) => {
+      // 判断用户是否已经授权用户信息
+      wx.getSetting({
+        success: resSetData => {
+          if (resSetData.authSetting['scope.userInfo']) { // 已授权
+            this.globalData.hasAuthorize = true
+            // 可以直接调用 getUserInfo 获取头像昵称
+            wx.getUserInfo({
+              success: resUserInfo => {
+                this.globalData.wxUserInfo = resUserInfo.userInfo
+                resolve({
+                  msgCode: 2, // 1：未授权；2：已授权
+                  message: '已授权'
+                })
+              }
+            })
+          } else { // 未授权
+            resolve({
+              msgCode: 1, // 1：未授权；2：已授权
+              message: '未授权'
+            })
+          }
+        }
+      })
+    })
+  }
   
 })
